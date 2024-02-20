1707909f5194185bfb61d4103e52a4c81719afa5
Mohammad Rezaei <mhdrezaei990@gmail.com>
2024-02-20 12:43:47 +0330

    change collection count function

--- controller/sunmmaryController.js
+++ controller/sunmmaryController.js
@@ -1,7 +1,7 @@
 const Transaction = require("../models/transaction")
 
 exports.allTransactions = async (req, res, next) => {
-    const count = await Transaction.find({})
+    const count = await Transaction.find({}).count()
         console.log( "Number of transactions:", count );
         res.status(200).json({
             success: true,
@@ -10,29 +10,29 @@ exports.allTransactions = async (req, res, next) => {
     
 }
 exports.successTransactions = async (req, res, next) => {
-    const count = await Transaction.find({status : "success"})
+    const count = await Transaction.find({status : "success"}).count()
         console.log( "Number of success transactions:", count );
         res.status(200).json({
             success: true,
-            count: count.count()
+            count: count
           });
     
 }
 exports.failedTransactions = async (req, res, next) => {
-    const count = await Transaction.find({status : "failed"})
+    const count = await Transaction.find({status : "failed"}).count()
         console.log( "Number of failed transactions:", count );
         res.status(200).json({
             success: true,
-            count: count.count()
+            count: count
           });
     
 }
 exports.unknownTransactions = async (req, res, next) => {
-    const count = await Transaction.find({status : "unknown"})
+    const count = await Transaction.find({status : "unknown"}).count()
         console.log( "Number of unknown transactions:", count );
         res.status(200).json({
             success: true,
-            count: count.count()
+            count: count
           });
     
 }
